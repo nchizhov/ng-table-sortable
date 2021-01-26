f17035735f131d39d0415bf8b6108a509b14d1f8
nikolay <nikolay@inok.ru>
2021-01-26 18:22:01 +0200

    Update packages compatible Angular@9

--- projects/ng-table-sortable/src/public_api.ts
+++ projects/ng-table-sortable/src/public_api.ts
@@ -5,4 +5,5 @@
 export * from './lib/ng-table-sortable.service';
 export * from './lib/ng-table-sortable.component';
 export * from './lib/ng-table-sortable.module';
+export * from './lib/ng-table-sortable.directive';
 export * from './lib/utils';
